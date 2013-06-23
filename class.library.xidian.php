82905650189281492ae7122c40c590889f53d35e
spdf <spdf@live.vom>
2013-06-23 19:45:13 +0800

    add: bookshistory
    改用正则表达式

--- class.library.xidian.php
+++ class.library.xidian.php
@@ -1,8 +1,9 @@
 <?php
 /*
-  	$options = array('id' => '00160450', 'password' => 'ACDsee123');
+		$options = array('id' => '------', 'password' => '------');
 		$lib = new Library_xidian($options);
 		$list = $lib->getrentbook();
+		$his = $lib->gethistorybook();
 */?>
 <?php
 	class Library_xidian{
@@ -11,12 +12,13 @@
 		public $RENT_DIR = "/items";
 		public $HISTORY_DIR = "/readinghistory";
 		
-		private $cookie_file;	
+		private $cookie_file;
 		
 		public $id;
 		public $password;
 		private $userdir;
 		private $booklist;
+		private $historybooklist;
 		
 		public function __construct($options){
 			$this->id = $options['id'];
@@ -47,7 +49,8 @@
 			$this->redirection_render($file_contents);
 		}
 		private function redirection_render($respond_header){
-			if (preg_match("/\d{7}(?=\/top)/", $respond_header, $matches)) {//匹配"/top"之前的7位数字
+			//匹配"/top"之前的7位数字
+			if (preg_match("/\d{7}(?=\/top)/", $respond_header, $matches)) {
 				$this->userdir = $matches[0];
 			} else {
 				$this->userdir = 'error';
@@ -62,22 +65,42 @@
 			curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file);
 			$file_contents = curl_exec($ch);
 			//echo $file_contents;
+			//echo "<hr/>";
 			curl_close($ch);
 			return $this->booklist_render($file_contents);
 		}
 		private function booklist_render($file_contents){
 			require "simple_html_dom.php";
 			$booktable = str_get_html($file_contents);
-			$titlelist = $booktable->find('td[class=patFuncTitle]');
-			$timelist = $booktable->find('td[class=patFuncStatus]');
-			for($i=0;$i<sizeof($titlelist);$i++){
-				$titletext = $titlelist[$i]->plaintext;
-				$timetext = $timelist[$i]->plaintext;
-				preg_match("/\d+-\d+-\d+/",$timetext,$time);
-				$title=$titletext;
+			//$titlelist = $booktable->find('td[class=patFuncTitle]');//(?<=chx"> ).+(?=</a></label>)
+			preg_match_all("/(?<=chx\"> ).+(?=<\/a><\/label>)/", $file_contents, $titlelist, PREG_PATTERN_ORDER);
+			//$timelist = $booktable->find('td[class=patFuncStatus]');//(?<=到期 )\d{2}-\d{2}-\d{2}
+			preg_match_all("/(?<=到期 )\d{2}-\d{2}-\d{2}/", $file_contents, $timelist, PREG_PATTERN_ORDER);
+			for($i=0;$i<sizeof($titlelist[0]);$i++){
+				$title = $titlelist[0][$i];
+				$time = $timelist[0][$i];
 				$this->booklist[$i] = array('title'=>$title,'time'=>$time,);
 			}
 			return $this->booklist;
+		}
+		public function gethistorybook(){
+			//get history  books
+			$get_url = $this->BASE_URL.$this->userdir.$this->HISTORY_DIR;
+			$ch = curl_init($get_url);
+			curl_setopt($ch, CURLOPT_HEADER, 0);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file);
+			$file_contents = curl_exec($ch);
+			//echo $file_contents;
+			//echo "<hr/>";
+			curl_close($ch);
+			return $this->historybooklist_render($file_contents);
+		}
+		private function historybooklist_render($file_contents){
+			//(?<=chx">).*(?=</a>)
+			preg_match_all("/(?<=chx\">).*(?=<\/a>)/", $file_contents, $list, PREG_PATTERN_ORDER);
+			$this->historybooklist = $list[0];
+			return $this->historybooklist;
 		}
 		public function getuserdir(){
 			return $this->userdir;
